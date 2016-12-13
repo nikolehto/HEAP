3303192989333aa52caaecc21b1bbcd1225c93ba
ailmari <Arttu.Niemela@student.oulu.fi>
2016-12-13 18:11:19 +0200

    Add files via upload

---
+++ Assets/movementScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movementScript : MonoBehaviour {
+    public float rotationSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed);
+	}
+}
